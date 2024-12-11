f1e5c825a77facd79127ffa148f257f291f0b3e4
shourya-r <rajputshourya7@gmail.com>
2024-12-11 10:53:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center text-orange-500 mt-5">
+        Password Generator
+      </h1>
+    </>
+  );
+}
+
+export default App;
